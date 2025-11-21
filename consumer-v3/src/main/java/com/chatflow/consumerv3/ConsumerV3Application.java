e967181588af336b1427fbb3e20a34276a518bf8
Jeremy <jeremykaku@outlook.com>
2025-11-20 16:30:07 -0800

    Add new consumer-v3 project

---
+++ consumer-v3/src/main/java/com/chatflow/consumerv3/ConsumerV3Application.java
@@ -0,0 +1,13 @@
+package com.chatflow.consumerv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerV3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ConsumerV3Application.class, args);
+  }
+
+}
